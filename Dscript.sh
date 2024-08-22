892ed423e9e662a7904c9e201f1ae0bdab688c3c
GOKULKRISHNA <gkmail2004@gmail.com>
2024-08-22 15:27:30 +0530

    Stage 12 [Idle program edit Assignment]

--- Dscript.sh
+++ Dscript.sh
@@ -2,6 +2,9 @@ cd $HOME/myexpos/xfs-interface
 ./xfs-interface << EOF
 load --os ../spl/spl_progs/startup.xsm
 load --init ../expl/samples/init.xsm
+load --idle ../expl/samples/idle.xsm
+load --int=timer ../spl/spl_progs/timer.xsm
+load --library ../expl/library.lib
 exit
 EOF
 
