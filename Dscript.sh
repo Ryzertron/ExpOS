ac0cffdb4249d876f69ceb6b8c0a537313a9b8b5
GOKULKRISHNA <gkmail2004@gmail.com>
2024-08-28 16:49:05 +0530

    Stage 13 [Boot module]

--- Dscript.sh
+++ Dscript.sh
@@ -2,6 +2,7 @@ cd $HOME/myexpos/xfs-interface
 ./xfs-interface << EOF
 load --os ../spl/spl_progs/startup.xsm
 load --init ../expl/samples/init.xsm
+load --module 7 ../spl/spl_progs/mod7boot.xsm
 exit
 EOF
 
