8364c688aae2f41b670fafe374b5196751752583
GOKULRISHNA V <Ryzertron@users.noreply.github.com>
2024-09-05 17:44:41 +0530

    Stage 15 Assignment 1 [Breakpoints]

--- Dscript.sh
+++ Dscript.sh
@@ -9,17 +9,19 @@ cd $HOME/myexpos/spl
 ./spl spl_progs/startup.spl
 ./spl spl_progs/mod7boot.spl
 ./spl spl_progs/timer.spl
+./spl spl_progs/mod0resourcemanager.spl
+./spl spl_progs/mod4devicemanager.spl
 cd $HOME/myexpos/xfs-interface
 ./xfs-interface << EOF
 load --idle ../expl/samples/idle.xsm
 load --int=timer ../spl/spl_progs/timer.xsm
 load --int=7 ../spl/spl_progs/int7.xsm
 load --int=10 ../spl/spl_progs/int10.xsm
+load --module 0 ../spl/spl_progs/mod0resourcemanager.xsm
+load --module 4 ../spl/spl_progs/mod4devicemanager.xsm
 load --module 5 ../spl/spl_progs/mod5scheduler.xsm
 load --module 7 ../spl/spl_progs/mod7boot.xsm
 load --library ../expl/library.lib
-rm even.xsm
-load --exec ../expl/samples/even.xsm
 load --os ../spl/spl_progs/startup.xsm
 load --init ../expl/samples/init.xsm
 exit
